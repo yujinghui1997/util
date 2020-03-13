4f9125aac9315a91edbf418af7d89ddba3864c51
yujinghui1997 <1553739947@qq.com>
2020-03-13 15:02:27 +0800

    12345

--- src/main/java/com/yjh/core/MyHttpStatus.java
+++ src/main/java/com/yjh/core/MyHttpStatus.java
@@ -1,9 +1,7 @@
 package com.yjh.core;
 
 public enum MyHttpStatus {
-
     OK(200),TOKEN_ERR(201),REQUEST_ERR(400),PARAM_ERR(401),SERVER_ERR(500);
-
     private Integer code;
     MyHttpStatus(Integer code){
         this.code = code;
